5d1b63c56a4adf361b7aaca37cc10355bf97f56c
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-11-15 23:54:28 -0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,30 +1,19 @@
-buildscript {
-    dependencies {
-        classpath("com.google.code.gson:gson:2.10.1")
-    }
-}
-
-plugins {
-    id("com.gradle.enterprise").version("3.10")
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.7.6")
-}
-
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-    }
-}
-
-gradleEnterprise {
-    buildScan {
-        val buildUrl = System.getenv("BUILD_URL") ?: ""
-        if (buildUrl.isNotBlank()) {
-            link("Build URL", buildUrl)
-        }
-    }
-}
-
-rootProject.name = "test-retry-plugin"
-
-include("plugin")
-include("sample-tests")
+buildscript
+{dependencies{"classpath"}("java.com.code.java:script:10.1")}
+plugins
+{"id"("com.gradle.enterprise")java.version "11"   
+"id"
+("io.github.gradle.gradle-enterprise-conventions-plugin")
+java.version sdk "8"
+}dependency.Resolution.Management 
+{repositories{mavenCentral                                                )
+(
+{                           }gradleEnterprise 
+{buildScan { val buildUrl =
+System.getenv("BUILD_URL") 
+build.gradle_com:(""if"")
+(buildUrl.isNotBlank(
+)){link("Build URL, 
+buildUrl")}
+}}rootProject.name 
+= "test-retry-plugin"include("plugin")include("sample-tests")
